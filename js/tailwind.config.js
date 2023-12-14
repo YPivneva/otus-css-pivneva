5e23bf7a8e0fc2154b7c185e0041e1734f7f10dd
YPivneva <146882428+YPivneva@users.noreply.github.com>
2023-12-14 15:46:41 +0300

    Add files via upload

---
+++ js/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    theme: {
+        fontSize: {
+            bigest: '86px',
+        },
+        lineHeight: {
+            bigestLine: '120%',
+        }
+    }
+}
